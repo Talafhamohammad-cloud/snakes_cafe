cce1fce284c4529acc68d58b1b5d2fcf9ab887d8
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-07-25 17:01:34 +0300

    second commit

---
+++ snakes_cafe/snakes_cafe.py
@@ -0,0 +1,39 @@
+print ("""**************************************\n**    Welcome to the Snakes Cafe!   **\n**    Please see our menu below.    **\n** To quit at any time, type "quit" **\n**************************************""")
+print(""" 
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+""")
+print("""
+***********************************
+** What would you like to order? **
+***********************************
+""")
+user_order= input(">")
+print(user_order)
+
+
+
+
+
