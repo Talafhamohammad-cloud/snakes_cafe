f34d2198773bdf5c68c7cb1dc4bc139c69aa1680
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-07-26 00:57:10 +0300

    finish the lab-01 last version

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,37 +1,55 @@
-print ("""**************************************\n**    Welcome to the Snakes Cafe!   **\n**    Please see our menu below.    **\n** To quit at any time, type "quit" **\n**************************************""")
-print(""" 
+print("""
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
 Appetizers
 ----------
 Wings
 Cookies
 Spring Rolls
-
 Entrees
 -------
 Salmon
 Steak
 Meat Tornado
 A Literal Garden
-
 Desserts
 --------
 Ice Cream
 Cake
 Pie
-
 Drinks
 ------
 Coffee
 Tea
 Unicorn Tears
-""")
-print("""
 ***********************************
 ** What would you like to order? **
 ***********************************
 """)
-user_order= input(">")
-print(user_order)
+menu = ["Wings", "Cookies", "Spring Rolls", "Salmon", "Steak", "Meat Tornado",
+        "A Literal Garden", "Ice Cream", "Cake", "Pie", "Coffee", "Tea", "Unicorn Tears"]
+your_order=[]        
+i = 1
+order = input(">")
+print(f"{i} order of {order} have been added to your meal ")
+your_order.append(order)
+
+while True:
+  print("would you like to order any things else")
+  order = input(">")
+  if order.lower() == "n":
+    break
+  i += 1
+  print(f"{i} order of {order}  have been added to your meal")
+  your_order.append(order)
+  
+print(f'your order is {your_order}')
+print("thank you\n your order will be ready soon")  
+
 
 
 
